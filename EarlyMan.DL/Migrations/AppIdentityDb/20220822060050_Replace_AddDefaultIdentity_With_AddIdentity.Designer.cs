64a184fd6d1a5aa4cec2a62c9e3918e4726d228b
David Okeke <davidokeke.c@gmail.com>
2022-08-27 20:19:01 +0100

    Add support for role based Identity

--- EarlyMan.DL/Migrations/AppIdentityDb/20220822060050_Replace_AddDefaultIdentity_With_AddIdentity.Designer.cs
+++ EarlyMan.DL/Migrations/AppIdentityDb/20220822060050_Replace_AddDefaultIdentity_With_AddIdentity.Designer.cs
@@ -4,6 +4,7 @@ using EarlyMan.DL.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace EarlyMan.DL.Migrations.AppIdentityDb
 {
     [DbContext(typeof(AppIdentityDbContext))]
-    partial class AppIdentityDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220822060050_Replace_AddDefaultIdentity_With_AddIdentity")]
+    partial class Replace_AddDefaultIdentity_With_AddIdentity
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
