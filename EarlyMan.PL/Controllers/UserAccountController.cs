16bdefd246104734e0f2a6e6b3b9ee43d71f9db6
David Okeke <davidokeke.c@gmail.com>
2022-08-27 20:25:52 +0100

    Allow user to change password

---
+++ EarlyMan.PL/Controllers/UserAccountController.cs
@@ -0,0 +1,34 @@
+﻿using EarlyMan.DL.Entities.Identity;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EarlyMan.PL.Controllers
+{
+    [Authorize]
+    public class UserAccountController : Controller
+    {
+        private readonly UserManager<ApplicationUser> usrMgr;
+
+        public UserAccountController(UserManager<ApplicationUser> usrMgr)
+        {
+            this.usrMgr = usrMgr;
+        }
+        public IActionResult Index(bool pwd_ChangeSuccess, bool account_ChangeSuccess)
+        {
+            if (pwd_ChangeSuccess == true)
+            {
+                ViewBag.pwd_ChangeSuccess = true;
+            }
+
+            else if(account_ChangeSuccess == true)
+            {
+                ViewBag.account_ChangeSuccess = true;
+            }
+
+
+            return View();
+
+        }
+    }
+}
